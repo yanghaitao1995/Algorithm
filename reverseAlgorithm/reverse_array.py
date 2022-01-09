e80a278176a00d456191cabfd6e6f504a0456fda
yanghaitao <yanghaitao1995@aliyun.com>
2022-01-09 19:29:24 +0800

    The root commit for reverse_array.py

---
+++ reverseAlgorithm/reverse_array.py
@@ -0,0 +1,15 @@
+# -*- coding=utf-8 -*-
+# reverse_array.py
+
+"""
+Demo:
+     array reverse based on python
+"""
+
+# initialize an array
+array = [1,2,3,4,5,6,7,8,9,10]
+# reverse the array
+# array = array[::-1]
+array.sort(reverse=True)
+print(array)
+
